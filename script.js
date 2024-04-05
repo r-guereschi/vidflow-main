8b3644a6decf26a5c9115fbd27ea54e137eed966
raphael guereschi <83614889+r-guereschi@users.noreply.github.com>
2024-04-05 10:10:51 -0300

    filtragem pronta

--- script.js
+++ script.js
@@ -28,4 +28,22 @@ buscarEMostrarVideos();
 
 const barraDePesquisa = document.querySelector('.pesquisar__input');
 
-barraDePesquisa
+barraDePesquisa.addEventListener('input', filtrarPesquisa)
+
+function filtrarPesquisa() {
+   const videos = document.querySelectorAll('.videos__item')
+   if (barraDePesquisa.value != "") {
+      for(let video of videos){
+         let titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+         let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+         if (!titulo.includes(valorFiltro)) {
+            video.style.display = "none"
+         } else {
+            video.style.display = "block"
+         }
+      }
+   } else {
+      video.style.display = 'block'
+   }
+}
